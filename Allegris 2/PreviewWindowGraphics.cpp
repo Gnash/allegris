ab4d285fcd04adc6ec77cb09a9597dfeec7cae2a
Danel <Daniel.Wilken@live.com>
2013-07-06 16:20:05 +0200

    Added a different BG color to the preview part

--- Allegris 2/PreviewWindowGraphics.cpp
+++ Allegris 2/PreviewWindowGraphics.cpp
@@ -1,5 +1,7 @@
 #include "PreviewWindowGraphics.h"
 #include "PreviewWindow.h"
+#include "globals.h"
+#include <allegro5/allegro_primitives.h>
 
 
 PreviewWindowGraphics::PreviewWindowGraphics(void) {
@@ -20,6 +22,7 @@ bool PreviewWindowGraphics::draw(void) {
 	bool success = true;
 	int x = getPreviewWindow()->getAbsoluteXPos();
 	int y = getPreviewWindow()->getAbsoluteYPos();
+	al_draw_filled_rectangle(x, y, x + PREVIEW_WINDOW_WIDTH, y + SCREEN_HEIGHT, al_map_rgb(20, 40, 20));
 	al_draw_bitmap(backgroundImage, x, y, 0);
 	success = success && getPreviewWindow()->getNextBlock()->draw();
 	int bla = SCREEN_WIDTH;
